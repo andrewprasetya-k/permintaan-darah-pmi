7903aca0407e1c2064065a5bf8595826c05c7f7e
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-03-30 13:25:09 +0700

    chore: format rumah_sakit.go model

--- backend/models/rumah_sakit.go
+++ backend/models/rumah_sakit.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type RumahSakit struct {
-	RSID      string  `gorm:"column:rs_id;primaryKey" json:"rumahSakitId"`
-	RSNama    string  `gorm:"column:rs_nama" json:"nama"`
-	RSNoTelp  string  `gorm:"column:rs_no_telp" json:"nomorTelepon"`
-	RSAlamat  string  `gorm:"column:rs_alamat" json:"alamat"`
-	RSEmail   *string `gorm:"column:rs_email;unique" json:"email,omitempty"`
-	RSPassword *string `gorm:"column:rs_password" json:"password,omitempty"`
-	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
-	UpdatedAt time.Time `gorm:"column:updated_at" json:"updatedAt"`
-	DeletedAt *time.Time `gorm:"column:deleted_at" json:"deletedAt,omitempty"`
+	RSID       string     `gorm:"column:rs_id;primaryKey" json:"rumahSakitId"`
+	RSNama     string     `gorm:"column:rs_nama" json:"nama"`
+	RSNoTelp   string     `gorm:"column:rs_no_telp" json:"nomorTelepon"`
+	RSAlamat   string     `gorm:"column:rs_alamat" json:"alamat"`
+	RSEmail    *string    `gorm:"column:rs_email;unique" json:"email,omitempty"`
+	RSPassword *string    `gorm:"column:rs_password" json:"password,omitempty"`
+	CreatedAt  time.Time  `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt  time.Time  `gorm:"column:updated_at" json:"updatedAt"`
+	DeletedAt  *time.Time `gorm:"column:deleted_at" json:"deletedAt,omitempty"`
 }
