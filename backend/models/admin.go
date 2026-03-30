28c6b98debfbc98c41bd72b2e5df7b12f77d1b9b
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-03-30 10:45:03 +0700

    refactor(admin): change json key

--- backend/models/admin.go
+++ backend/models/admin.go
@@ -1,16 +1,16 @@
 package models
 
 type Admin struct {
-	AdminID      string         `gorm:"column:admin_id;primaryKey" json:"admin_id"`
-	AdminUsername string        `gorm:"column:admin_username;unique" json:"admin_username"`
-	AdminPassword string        `gorm:"column:admin_password" json:"admin_password"`
-	DeletedAt     *int64        `gorm:"column:deleted_at" json:"deleted_at,omitempty"`	
-	IsDeleted     bool          `gorm:"column:is_deleted;default:false" json:"is_deleted"`	
-	UpdatedAt     int64         `gorm:"column:updated_at" json:"updated_at"`	
-	CreatedAt     int64         `gorm:"column:created_at" json:"created_at"`	
-	AdminRole     AdminRoleEnum `gorm:"column:admin_role;default:admin" json:"admin_role"`	
-	AdminEmail    string        `gorm:"column:admin_email;unique" json:"admin_email"`	
-	AdminNama     string        `gorm:"column:admin_nama" json:"admin_nama"`
+	AdminID      string         `gorm:"column:admin_id;primaryKey" json:"adminId"`
+	AdminUsername string        `gorm:"column:admin_username;unique" json:"adminUsername"`
+	AdminPassword string        `gorm:"column:admin_password" json:"adminPassword"`
+	DeletedAt     *int64        `gorm:"column:deleted_at" json:"deletedAt,omitempty"`	
+	IsDeleted     bool          `gorm:"column:is_deleted;default:false" json:"isDeleted"`	
+	UpdatedAt     int64         `gorm:"column:updated_at" json:"updatedAt"`	
+	CreatedAt     int64         `gorm:"column:created_at" json:"createdAt"`	
+	AdminRole     AdminRoleEnum `gorm:"column:admin_role;default:admin" json:"adminRole"`	
+	AdminEmail    string        `gorm:"column:admin_email;unique" json:"adminEmail"`	
+	AdminNama     string        `gorm:"column:admin_nama" json:"adminName"`
 
 
 
