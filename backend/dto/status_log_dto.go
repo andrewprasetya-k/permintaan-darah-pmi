2982e7ec42b719e1e539db076202b6a76a7393f0
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-03-30 13:25:32 +0700

    feat: add status_log_dto.go

---
+++ backend/dto/status_log_dto.go
@@ -0,0 +1,30 @@
+package dto
+
+import (
+	"backend/models"
+	"time"
+)
+
+type CreateStatusLogRequest struct {
+	LogID       string                      `json:"logId" binding:"required"`
+	LogPdID     string                      `json:"permintaanDarahId" binding:"required"`
+	LogAdminID  string                      `json:"adminId" binding:"required"`
+	LogStatusTo models.PermintaanStatusEnum `json:"statusTo" binding:"required,oneof=diterima diproses bisa_diambil selesai dibatalkan"`
+	LogNotes    *string                     `json:"notes,omitempty"`
+}
+
+type UpdateStatusLogRequest struct {
+	LogPdID     string                      `json:"permintaanDarahId" binding:"required"`
+	LogAdminID  string                      `json:"adminId" binding:"required"`
+	LogStatusTo models.PermintaanStatusEnum `json:"statusTo" binding:"required,oneof=diterima diproses bisa_diambil selesai dibatalkan"`
+	LogNotes    *string                     `json:"notes,omitempty"`
+}
+
+type StatusLogResponse struct {
+	LogID       string                      `json:"logId"`
+	LogPdID     string                      `json:"permintaanDarahId"`
+	LogAdminID  string                      `json:"adminId"`
+	LogStatusTo models.PermintaanStatusEnum `json:"statusTo"`
+	LogNotes    *string                     `json:"notes,omitempty"`
+	CreatedAt   time.Time                   `json:"createdAt"`
+}
