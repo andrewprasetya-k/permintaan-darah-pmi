3a4774f2f0dc50fe78f3e6fe61f871b9ae2fa57a
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-03-30 10:53:59 +0700

    refactor(komponen darah): change json key

--- backend/models/komponen_darah.go
+++ backend/models/komponen_darah.go
@@ -1,8 +1,8 @@
 package models
 
 type KomponenDarah struct {
-	KomID    int    `gorm:"column:kom_id;primaryKey;autoIncrement" json:"kom_id"`
-	KomNama  string `gorm:"column:kom_nama" json:"kom_nama"`
-	KomKode  string `gorm:"column:kom_kode;unique" json:"kom_kode"`
-	IsActive bool   `gorm:"column:is_active;default:true" json:"is_active"`
+	KomID    int    `gorm:"column:kom_id;primaryKey;autoIncrement" json:"komponenId"`
+	KomNama  string `gorm:"column:kom_nama" json:"komponenDarah"`
+	KomKode  string `gorm:"column:kom_kode;unique" json:"komponenKode"`
+	IsActive bool   `gorm:"column:is_active;default:true" json:"isActive"`
 }
