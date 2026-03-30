db58e04ba8ffb1c746842cdc788db332f0b957e0
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-03-30 13:25:06 +0700

    chore: format permintaan_darah.go model

--- backend/models/permintaan_darah.go
+++ backend/models/permintaan_darah.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type PermintaanDarah struct {
-	PDID           string                `gorm:"column:pd_id;primaryKey" json:"permintaanDarahId"`
-	PDRsID         string                `gorm:"column:pd_rs_id" json:"rumahSakitId"`
-	PDPsnID        string                `gorm:"column:pd_psn_id" json:"pasienId"`
-	PDStatus       PermintaanStatusEnum  `gorm:"column:pd_status;default:diterima" json:"status"`
-	PDCancelReason *string               `gorm:"column:pd_cancel_reason" json:"cancelReason,omitempty"`
+	PDID            string               `gorm:"column:pd_id;primaryKey" json:"permintaanDarahId"`
+	PDRsID          string               `gorm:"column:pd_rs_id" json:"rumahSakitId"`
+	PDPsnID         string               `gorm:"column:pd_psn_id" json:"pasienId"`
+	PDStatus        PermintaanStatusEnum `gorm:"column:pd_status;default:diterima" json:"status"`
+	PDCancelReason  *string              `gorm:"column:pd_cancel_reason" json:"cancelReason,omitempty"`
 	PDTglPermintaan time.Time            `gorm:"column:pd_tgl_permintaan" json:"tanggalPermintaan"`
-	CreatedAt      time.Time             `gorm:"column:created_at" json:"createdAt"`
-	UpdatedAt      time.Time             `gorm:"column:updated_at" json:"updatedAt"`
-	DeletedAt      *time.Time            `gorm:"column:deleted_at" json:"deletedAt,omitempty"`
+	CreatedAt       time.Time            `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt       time.Time            `gorm:"column:updated_at" json:"updatedAt"`
+	DeletedAt       *time.Time           `gorm:"column:deleted_at" json:"deletedAt,omitempty"`
 }
