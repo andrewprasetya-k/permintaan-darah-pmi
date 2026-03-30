e3f65301ea764126f557451c40f07aad5f8f3929
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-03-30 11:17:32 +0700

    updated gitignore

---
+++ backend/config/database.go
@@ -0,0 +1,27 @@
+package config
+package config
+
+import (
+	"errors"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDBFromEnv() (*gorm.DB, error) {
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		return nil, errors.New("DB_DSN is not set")
+	}
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
