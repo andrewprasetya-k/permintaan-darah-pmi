d376c24683146f8bcb9f6b8a92757f7aa8b74c08
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-03-30 13:25:12 +0700

    chore: format status_logs.go model

--- backend/models/status_logs.go
+++ backend/models/status_logs.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type StatusLog struct {
-	LogID      string                `gorm:"column:log_id;primaryKey" json:"logId"`
-	LogPdID    string                `gorm:"column:log_pd_id" json:"permintaanDarahId"`
-	LogAdminID string                `gorm:"column:log_admin_id" json:"adminId"`
+	LogID       string               `gorm:"column:log_id;primaryKey" json:"logId"`
+	LogPdID     string               `gorm:"column:log_pd_id" json:"permintaanDarahId"`
+	LogAdminID  string               `gorm:"column:log_admin_id" json:"adminId"`
 	LogStatusTo PermintaanStatusEnum `gorm:"column:log_status_to" json:"statusTo"`
-	LogNotes   *string               `gorm:"column:log_notes" json:"notes,omitempty"`
-	CreatedAt  time.Time             `gorm:"column:created_at" json:"createdAt"`
+	LogNotes    *string              `gorm:"column:log_notes" json:"notes,omitempty"`
+	CreatedAt   time.Time            `gorm:"column:created_at" json:"createdAt"`
 }
