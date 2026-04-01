b1a451b246b0a140f5121000bfae94dbfab0a99f
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-04-01 14:41:39 +0700

    feat: add rs_username field to rumah_sakit model

--- backend/models/rumah_sakit.go
+++ backend/models/rumah_sakit.go
@@ -8,7 +8,8 @@ type RumahSakit struct {
 	RSNoTelp   string     `gorm:"column:rs_no_telp" json:"nomorTelepon"`
 	RSAlamat   string     `gorm:"column:rs_alamat" json:"alamat"`
 	RSEmail    *string    `gorm:"column:rs_email;unique" json:"email,omitempty"`
-	RSPassword *string    `gorm:"column:rs_password" json:"password,omitempty"`
+	RSUsername string     `gorm:"column:rs_username;unique" json:"username"`
+	RSPassword string    `gorm:"column:rs_password" json:"password"`
 	CreatedAt  time.Time  `gorm:"column:created_at" json:"createdAt"`
 	UpdatedAt  time.Time  `gorm:"column:updated_at" json:"updatedAt"`
 	DeletedAt  *time.Time `gorm:"column:deleted_at" json:"deletedAt,omitempty"`
