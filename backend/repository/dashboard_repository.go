76207542071594dd1d54ec9a29323200ad3cb508
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-04-01 11:49:05 +0700

    fix(dashbaord): fix join query

--- backend/repository/dashboard_repository.go
+++ backend/repository/dashboard_repository.go
@@ -19,12 +19,14 @@ func NewDashboardRepository(db *gorm.DB) DashboardRepository {
 func (r *dashboardRepository) StatusSummary(rumahSakitID string) ([]int, error) {
 	status := []int{0, 0, 0, 0, 0}
 	err := r.db.Table("permintaan_darah").
-		Select("status, COUNT(*) as count").
-		Where("rs_id = ?", rumahSakitID).
-		Group("status").
+		Select("pd_status as status, COUNT(*) as count").
+		Where("pd_rs_id = ?", rumahSakitID).
+		Group("pd_status").
 		Scan(&status).Error
 	if err != nil {
 		return nil, err
 	}
 	return status, nil
 }
+
+	
