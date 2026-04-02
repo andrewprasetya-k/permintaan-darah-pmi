fb41e6f02ad5b61e7feced96fcc7acfcd0077c3b
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-04-02 08:59:09 +0700

    feat(rumah sakit dto): modify dto

--- backend/dto/rumah_sakit_dto.go
+++ backend/dto/rumah_sakit_dto.go
@@ -7,7 +7,8 @@ type CreateRumahSakitRequest struct {
 	RSNoTelp   string  `json:"nomorTelepon" binding:"required"`
 	RSAlamat   string  `json:"alamat" binding:"required"`
 	RSEmail    *string `json:"email,omitempty" binding:"omitempty,email"`
-	RSPassword *string `json:"password,omitempty"`
+	RSUsername string  `json:"username" binding:"required"`
+	RSPassword string `json:"password" binding:"required"`
 }
 
 type UpdateRumahSakitRequest struct {
@@ -24,6 +25,7 @@ type RumahSakitResponse struct {
 	RSNoTelp   string     `json:"nomorTelepon"`
 	RSAlamat   string     `json:"alamat"`
 	RSEmail    *string    `json:"email,omitempty"`
+	RSUsername string  `json:"username" binding:"required"`
 	RSPassword *string    `json:"password,omitempty"`
 	CreatedAt  time.Time  `json:"createdAt"`
 	UpdatedAt  time.Time  `json:"updatedAt"`
