a0561acdfdf5b935fc75bc9deb67c4d48147b163
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-04-07 09:32:43 +0700

    feat(rbac): added rbac

---
+++ backend/middleware/rbac.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AdminOnly() gin.HandlerFunc{
+	return func (c *gin.Context) { 
+		userRole, exists := c.Get("userRole")
+		if !exists || userRole != "admin" {
+			c.AbortWithStatusJSON(403, gin.H{"error": "Forbidden: Admins only"})
+			return
+		}
+	}
+}
+
+func RumahSakitOnly() gin.HandlerFunc{
+	return func (c *gin.Context) { 
+		userRole, exists := c.Get("userRole")
+		if !exists || userRole != "rumah_sakit" {
+			c.AbortWithStatusJSON(403, gin.H{"error": "Forbidden: Rumah Sakit only"})
+			return
+		}
+	}
+}
+
+func AdminOrRumahSakit() gin.HandlerFunc{
+	return func (c *gin.Context) { 
+		userRole, exists := c.Get("userRole")
+		if !exists || (userRole != "admin" && userRole != "rumah_sakit") {
+			c.AbortWithStatusJSON(403, gin.H{"error": "Forbidden: Admins or Rumah Sakit only"})
+			return
+		}
+	}
+}
