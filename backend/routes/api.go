d91bdb75ec9842a48bf24e35b27b78806103b246
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-04-07 09:59:07 +0700

    refactor(api): sort by roles

--- backend/routes/api.go
+++ backend/routes/api.go
@@ -21,20 +21,12 @@ func RegisterAPIRoutes(
 ) {
 	api := r.Group("/api")
 
-	//utils
-	dashboard := api.Group("/dashboard")
-	dashboard.GET("/status-summary/:rumahSakitID", dashboardController.StatusSummary)
-
-	filter := api.Group("/filter")
-	filter.GET("/rumah-sakit/", rumahSakitController.GetDistinctRSNama)
-
+	// public routes
 	auth := api.Group("/auth")
-	auth.Use(middleware.AdminOnly())
 	auth.POST("/login/admin", authController.AdminLogin)
-	auth.Use(middleware.RumahSakitOnly())
 	auth.POST("/login/rumah-sakit", authController.RumahSakitLogin)
 
-	//main endpoints
+	// superadmin only
 	admins := api.Group("/admin")
 	admins.Use(middleware.SuperAdminOnly())
 	admins.POST("", adminController.Create)
@@ -44,8 +36,9 @@ func RegisterAPIRoutes(
 	admins.DELETE("/:id", adminController.Delete)
 	admins.PUT("/restore/:id", adminController.Restore)
 
+	// admin only
 	rumahSakit := api.Group("/rumah-sakit")
-	rumahSakit.Use(middleware.AdminOrRumahSakit())
+	rumahSakit.Use(middleware.AdminOnly())
 	rumahSakit.POST("", rumahSakitController.Create)
 	rumahSakit.GET("", rumahSakitController.GetAll)
 	rumahSakit.GET("/:id", rumahSakitController.GetByID)
@@ -62,26 +55,8 @@ func RegisterAPIRoutes(
 	komponen.DELETE("/:id", komponenController.Delete)
 	komponen.PUT("/activate/:id", komponenController.Activate)
 	komponen.PUT("/deactivate/:id", komponenController.Deactivate)
-	
-	permintaan := api.Group("/permintaan-darah")
-	permintaan.Use(middleware.AdminOnly())
-	permintaan.POST("", permintaanController.Create)
-	permintaan.GET("", permintaanController.GetAll)
-	permintaan.GET("/:id", permintaanController.GetByID)
-	permintaan.PUT("/:id", permintaanController.Update)
-	permintaan.DELETE("/:id", permintaanController.Delete)
-	permintaan.PUT("/restore/:id", permintaanController.Restore)
-	//approval endpoints
-	permintaan.PUT("/update/:id", permintaanController.UpdateStatus)
 
-	detail := api.Group("/detail-permintaan-darah")
-	detail.Use(middleware.AdminOrRumahSakit())
-	detail.POST("", detailController.Create)
-	detail.GET("", detailController.GetAll)
-	detail.GET("/:id", detailController.GetByID)
-	detail.PUT("/:id", detailController.Update)
-	detail.DELETE("/:id", detailController.Delete)
-
+	// admin only
 	statusLogs := api.Group("/status-logs")
 	statusLogs.Use(middleware.AdminOnly())
 	statusLogs.GET("", statusLogController.GetAll)
@@ -94,4 +69,31 @@ func RegisterAPIRoutes(
 	systemLogs.GET("/action/:action", systemAccessLogController.GetByAction)
 	systemLogs.GET("/table/:table", systemAccessLogController.GetByTargetTable)
 	systemLogs.GET("/target/:targetId", systemAccessLogController.GetByTargetID)
+
+	// admin/rumah sakit
+	permintaan := api.Group("/permintaan-darah")
+	permintaan.Use(middleware.AdminOrRumahSakit())
+	permintaan.POST("", permintaanController.Create)
+	permintaan.GET("", permintaanController.GetAll)
+	permintaan.GET("/:id", permintaanController.GetByID)
+	permintaan.PUT("/:id", permintaanController.Update)
+	permintaan.DELETE("/:id", permintaanController.Delete)
+	permintaan.PUT("/restore/:id", permintaanController.Restore)
+	permintaan.PUT("/update/:id", permintaanController.UpdateStatus)
+
+	detail := api.Group("/detail-permintaan-darah")
+	detail.Use(middleware.RumahSakitOnly())
+	detail.POST("", detailController.Create)
+	detail.GET("", detailController.GetAll)
+	detail.GET("/:id", detailController.GetByID)
+	detail.PUT("/:id", detailController.Update)
+	detail.DELETE("/:id", detailController.Delete)
+
+	filter := api.Group("/filter")
+	filter.Use(middleware.AdminOrRumahSakit())
+	filter.GET("/rumah-sakit/", rumahSakitController.GetDistinctRSNama)
+
+	dashboard := api.Group("/dashboard")
+	dashboard.Use(middleware.AdminOrRumahSakit())
+	dashboard.GET("/status-summary/:rumahSakitID", dashboardController.StatusSummary)
 }
