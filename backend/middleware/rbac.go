04098b8bac72a6612791a7cbf548d40d9c1c00e0
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-04-07 10:05:17 +0700

    refactor(rbac): update user role retrieval to use "userRole" key

--- backend/middleware/rbac.go
+++ backend/middleware/rbac.go
@@ -4,42 +4,46 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SuperAdminOnly() gin.HandlerFunc{
-	return func (c *gin.Context) { 
-		userRole, exists := c.Get("user")
+func SuperAdminOnly() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userRole, exists := c.Get("userRole")
 		if !exists || userRole != "superadmin" {
 			c.AbortWithStatusJSON(403, gin.H{"error": "Forbidden: Super Admins only"})
 			return
 		}
+		c.Next()
 	}
 }
 
-func AdminOnly() gin.HandlerFunc{
-	return func (c *gin.Context) { 
-		userRole, exists := c.Get("user")
-		if !exists || userRole != "admin" {
+func AdminOnly() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userRole, exists := c.Get("userRole")
+		if !exists || (userRole != "admin" && userRole != "superadmin") {
 			c.AbortWithStatusJSON(403, gin.H{"error": "Forbidden: Admins only"})
 			return
 		}
+		c.Next()
 	}
 }
 
-func RumahSakitOnly() gin.HandlerFunc{
-	return func (c *gin.Context) { 
-		userRole, exists := c.Get("user")
+func RumahSakitOnly() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userRole, exists := c.Get("userRole")
 		if !exists || userRole != "rumah_sakit" {
 			c.AbortWithStatusJSON(403, gin.H{"error": "Forbidden: Rumah Sakit only"})
 			return
 		}
+		c.Next()
 	}
 }
 
-func AdminOrRumahSakit() gin.HandlerFunc{
-	return func (c *gin.Context) { 
+func AdminOrRumahSakit() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		userRole, exists := c.Get("userRole")
-		if !exists || (userRole != "admin" && userRole != "rumah_sakit") {
+		if !exists || (userRole != "admin" && userRole != "superadmin" && userRole != "rumah_sakit") {
 			c.AbortWithStatusJSON(403, gin.H{"error": "Forbidden: Admins or Rumah Sakit only"})
 			return
 		}
+		c.Next()
 	}
 }
