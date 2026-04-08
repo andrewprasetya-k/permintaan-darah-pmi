8e5814f29f197ca4e7d180fdfb1d6aeac3f9e7d1
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-04-08 12:02:27 +0700

    build: added tailwind cdn and implemetnation

--- frontend-admin/src/views/HomeView.vue
+++ frontend-admin/src/views/HomeView.vue
@@ -5,5 +5,6 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </main>
 </template>
