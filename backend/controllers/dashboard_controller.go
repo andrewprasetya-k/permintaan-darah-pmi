21d6b2a4203d2c959a4304b3bce02e18b0733fa4
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-04-21 08:38:47 +0700

    feat(dashboard controller): make sure status summary is guarded with rumah sakit id for sutatus summary

--- backend/controllers/dashboard_controller.go
+++ backend/controllers/dashboard_controller.go
@@ -4,6 +4,7 @@ import (
 	"backend/services"
 	"backend/utils"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +19,20 @@ func NewDashboardController(service services.DashboardService) *DashboardControl
 
 func (ctl *DashboardController) StatusSummary(c *gin.Context) {
 	rumahSakitID := c.Param("rumahSakitID")
+	userID, _, userRole := utils.ExtractUserFromJWT(c)
+
+	if userRole == "rumah_sakit" {
+		if userID == nil || *userID == "" {
+			utils.SendError(c, http.StatusUnauthorized, "Invalid user ID in token")
+			return
+		}
+		rumahSakitID = *userID
+	}
+
+	if strings.EqualFold(rumahSakitID, "all") {
+		rumahSakitID = ""
+	}
+
 	resp, err := ctl.service.StatusSummary(rumahSakitID)
 	if err != nil {
 		utils.HandleError(c, err)
